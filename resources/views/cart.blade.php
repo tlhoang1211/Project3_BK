6a329937f9678abe5ef496b86e35922c0eb1bfea
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-11-05 12:09:55 +0700

    Add: add Shipment Detail after payment confirmation

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,6 +5,7 @@
 @endsection
 @section('specific_js')
     <script src="{{asset('assets/js/custome_select.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 @endsection
 @section('content')
     <main class="bg_gray">
@@ -111,21 +112,21 @@
                 </table>
 
 
-        {{--            <div class="row add_top_30 flex-sm-row-reverse cart_actions">--}}
-        {{--                <div class="col-sm-4 text-right">--}}
-        {{--                    <button type="button" class="btn_1 gray">Update Cart</button>--}}
-        {{--                </div>--}}
-        {{--                <div class="col-sm-8">--}}
-        {{--                    <div class="apply-coupon">--}}
-        {{--                        <div class="form-group form-inline">--}}
-        {{--                            <input type="text" name="coupon-code" value="" placeholder="Promo code"--}}
-        {{--                                   class="form-control">--}}
-        {{--                            <button type="button" class="btn_1 outline">Apply Coupon</button>--}}
-        {{--                        </div>--}}
-        {{--                    </div>--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        <!-- /cart_actions -->
+            {{--            <div class="row add_top_30 flex-sm-row-reverse cart_actions">--}}
+            {{--                <div class="col-sm-4 text-right">--}}
+            {{--                    <button type="button" class="btn_1 gray">Update Cart</button>--}}
+            {{--                </div>--}}
+            {{--                <div class="col-sm-8">--}}
+            {{--                    <div class="apply-coupon">--}}
+            {{--                        <div class="form-group form-inline">--}}
+            {{--                            <input type="text" name="coupon-code" value="" placeholder="Promo code"--}}
+            {{--                                   class="form-control">--}}
+            {{--                            <button type="button" class="btn_1 outline">Apply Coupon</button>--}}
+            {{--                        </div>--}}
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
+            <!-- /cart_actions -->
 
         </div>
         <!-- /container -->
@@ -142,16 +143,63 @@
                                 <span>Total    :</span> {{number_format($total_price, '0', '3', '.') . ' ₫'}}
                             </li>
                         </ul>
-                        <a href="cart-2.html" class="btn_1 full-width cart">Xác nhận thanh toán</a>
+
+                        {{--<a href="cart-2.html" class="btn_1 full-width cart">Xác nhận thanh toán</a>--}}
+                        <button type="button" class="btn_1 full-width cart" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                            Xác nhận thanh toán
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    @else
+        @else
         Hiện tại bạn không có sản phẩm nào trong giỏ hàng
         @endif
-        <!-- /box_cart -->
+    <!-- /box_cart -->
+
+        {{-- Order comfirmation--}}
+    <!-- Button trigger modal -->
+        {{--<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">--}}
+        {{--    Launch demo modal--}}
+        {{--</button>--}}
+
 
     </main>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header" style="background: #3A87AD; margin-left: 0">
+                    <h5 class="modal-title" id="exampleModalLabel" style="color: whitesmoke">Shipment Detail</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Họ và tên:</label>
+                            <input type="text" class="form-control" id="recipient-name" value="{{$account->fullName}}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Email:</label>
+                            <input type="email" class="form-control" id="recipient-name" value="{{$account->email}}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Số điện thoại</label>
+                            <input type="text" class="form-control" id="recipient-name" value="{{$account->phoneNumber}}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Address:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Place order</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <!--/main-->
 @endsection
