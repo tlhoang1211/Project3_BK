58f3fee3c8ce2efdb2c47fb078a7c71d0896e4d9
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-11-10 14:38:42 +0700

    Fix: data is not updated after placing new order

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,25 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
+use App\Account;
 
 class UserController extends Controller
 {
     public function orderList()
     {
         $account = session()->get("current_account");
-        $receipts = $account->receipts;
-//            ->map(function ($receipt, $key) {
-//            $receipt->orders->map(function ($order, $key) {
-//                return [
-//                    'name'     => Product::find($order->product_id)->name,
-//                    'volume'   => $order->volume,
-//                    'quantity' => $order->quantity,
-//                    'price'    => $order->price,
-//                    'date'     => $order->created_at,
-//                ];
-//            })
-//        });
+        $receipts = Account::find($account->id)->receipts;
+
         return view('purchase', compact('account', 'receipts'));
     }
 }
