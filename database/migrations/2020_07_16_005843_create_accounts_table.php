06ce2471b41a0e69cd1fa9bdc29914a3678d9272
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-10 01:52:47 +0700

    update: make attribute nullable case: register using social account

--- database/migrations/2020_07_16_005843_create_accounts_table.php
+++ database/migrations/2020_07_16_005843_create_accounts_table.php
@@ -16,16 +16,18 @@ class CreateAccountsTable extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('role_id')->default(1);
-            $table->foreignId('city_id');
+            $table->foreignId('city_id')->nullable();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('provided_id')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('password')->nullable();
             $table->string('fullName');
-            $table->string('sex');
-            $table->date('birthDate');
-            $table->string('phoneNumber');
-            $table->string('email_verified');
-            $table->integer('status');
-            $table->string('address');
+            $table->string('sex')->nullable();
+            $table->date('birthDate')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('email_verified')->nullable();
+            $table->integer('status')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
