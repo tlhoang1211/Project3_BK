87ab3ca81f778a0d86b1ccaa97852a26e7b302a3
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-11 22:35:30 +0700

    add: Facebook chat plugin

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
 
 <body>
 
-{{--<x-facebook-chat />--}}
 
 @include('layouts.menu_bar')
 
@@ -20,38 +19,6 @@
 <!-- Back to top button -->
 <div id="toTop"></div>
 
-<!-- Messenger Plugin chat Code -->
-<div id="fb-root"></div>
-
-<!-- Your Plugin chat code -->
-{{--<div id="fb-customer-chat" class="fb-customerchat">--}}
-{{--</div>--}}
-
-{{--<script>--}}
-{{--    var chatbox = document.getElementById('fb-customer-chat');--}}
-{{--    chatbox.setAttribute("page_id", "107025061828658");--}}
-{{--    chatbox.setAttribute("attribution", "biz_inbox");--}}
-{{--</script>--}}
-
-{{--<!-- Your SDK code -->--}}
-{{--<script>--}}
-{{--    window.fbAsyncInit = function () {--}}
-{{--        FB.init({--}}
-{{--            xfbml: true,--}}
-{{--            version: 'v12.0'--}}
-{{--        });--}}
-{{--    };--}}
-
-{{--    (function (d, s, id) {--}}
-{{--        var js, fjs = d.getElementsByTagName(s)[0];--}}
-{{--        if (d.getElementById(id)) return;--}}
-{{--        js = d.createElement(s);--}}
-{{--        js.id = id;--}}
-{{--        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';--}}
-{{--        fjs.parentNode.insertBefore(js, fjs);--}}
-{{--    }(document, 'script', 'facebook-jssdk'));--}}
-{{--</script>--}}
-
 <!-- COMMON SCRIPTS -->
 <script src={{asset('assets/js/common_scripts.min.js')}}></script>
 <script src={{asset('assets/js/main.js')}}></script>
@@ -60,5 +27,7 @@
 <script src={{asset('assets/js/SaveScrollPosition.js')}}></script>
 <!-- SPECIFIC SCRIPTS -->
 @yield('specific_js')
+
+<x-facebook-chat/>
 
 </body>
