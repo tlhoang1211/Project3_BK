9056621cafcf63117bd3ee6512bb872a7c666ee4
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-13 11:42:34 +0700

    remove Vercel config files

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '/../public/index.php';
-
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
