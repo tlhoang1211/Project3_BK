b63b1c7e83a73194cafccc233d35cecfe7851801
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-20 01:12:16 +0700

    fix: multiple toast noti display

--- resources/views/components/product/gid.blade.php
+++ resources/views/components/product/gid.blade.php
@@ -1,7 +1,7 @@
 @props(['products', 'category' => '', 'categoryLink' => '#'])
 
 @push('specific_js')
-    <script>
+	<script>
 
         $(".add-to-cart").on("click", function ()
         {
@@ -45,29 +45,32 @@
                     toastr.error(data.responseText);
                 }
             });
+
+            //Fix: multiple toast notifications (because of repeating adding onClick's handler for each x-product.gid usage)
+            $(this).stopPropagation();
         });
-    </script>
+	</script>
 @endpush
 
 <div class="row">
 
-    {{--Category--}}
-    @if ($category !== '')
-        <div class="row__category">
-            <a href="{{ $categoryLink }}">
-                <span>Nước hoa</span>
-                {{ $category }}
-                <i class="far fa-arrow-right"></i>
-            </a>
-        </div>
-    @endif
+	{{--Category--}}
+	@if ($category !== '')
+		<div class="row__category">
+			<a href="{{ $categoryLink }}">
+				<span>Nước hoa</span>
+				{{ $category }}
+				<i class="far fa-arrow-right"></i>
+			</a>
+		</div>
+	@endif
 
-    {{--Products--}}
-    @foreach($products as $product)
-        <div class="col-6 col-md-4 col-xl-3">
-            <x-product.grid-item :product="$product"/>
-        </div>
-    @endforeach
+	{{--Products--}}
+	@foreach($products as $product)
+		<div class="col-6 col-md-4 col-xl-3">
+			<x-product.grid-item :product="$product"/>
+		</div>
+	@endforeach
 
-    {{ $slot }}
+	{{ $slot }}
 </div>
