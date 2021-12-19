563bcc7a4701f810c3b7c56e4d8476fa3dd91086
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-20 01:04:39 +0700

    remove: old code

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,62 +1,51 @@
 @extends('layouts.master')
 @section('title')
-    Wanderlust
+	Wanderlust
 @endsection
 @section('specific_css')
-    <link href={{ asset('assets/css/scss/home.css') }} rel="stylesheet">
+	<link href={{ asset('assets/css/scss/home.css') }} rel="stylesheet">
 @endsection
 @section('specific_js')
-    <script src={{asset('assets/js/carousel-home.min.js')}}></script>
-    <script>
-        $(".buy").click(function ()
-        {
-            $(".bottom").addClass("clicked");
-        });
-
-        $(".remove").click(function ()
-        {
-            $(".bottom").removeClass("clicked");
-        });
-    </script>
+	<script src={{asset('assets/js/carousel-home.min.js')}}></script>
 @endsection
 @section('content')
-    <div id="page">
-        <!-- /header -->
-        <main>
-            @include('pages.home._carousel')
-            @include('pages.home._banners')
+	<div id="page">
+		<!-- /header -->
+		<main>
+			@include('pages.home._carousel')
+			@include('pages.home._banners')
 
-            {{--Product grid--}}
-            <div class="container margin_60_35">
+			{{--Product grid--}}
+			<div class="container margin_60_35">
 
-                <div class="main_title">
-                    <h2>Được đánh giá tốt nhất</h2>
-                    <span>Các sản phẩm</span>
-                    <p>Các thương hiệu nước hoa được feedback nhiều nhất tại Việt Nam</p>
-                </div>
+				<div class="main_title">
+					<h2>Được đánh giá tốt nhất</h2>
+					<span>Các sản phẩm</span>
+					<p>Các thương hiệu nước hoa được feedback nhiều nhất tại Việt Nam</p>
+				</div>
 
-                <x-product.gid :products="$products_male"
-                               category="Nam Giới"
-                               :categoryLink="route('male_product')"
-                />
+				<x-product.gid :products="$products_male"
+				               category="Nam Giới"
+				               :categoryLink="route('product_search', ['sex' => 'Nam'])"
+				/>
 
-                <x-product.gid :products="$products_female"
-                               category="Nữ Giới"
-                               :categoryLink="route('female_product')"
-                />
+				<x-product.gid :products="$products_female"
+				               category="Nữ Giới"
+				               :categoryLink="route('product_search', ['sex' => 'Nữ'])"
+				/>
 
-                <x-product.gid :products="$products_unisex"
-                               category="Phi Giới Tính"
-                               :categoryLink="route('unisex_product')"
-                />
-            </div>
+				<x-product.gid :products="$products_unisex"
+				               category="Phi Giới Tính"
+				               :categoryLink="route('product_search', ['sex' => 'Phi giới tính'])"
+				/>
+			</div>
 
-            @include('pages.home._feature')
-            @include('pages.home._brands')
-            @include('pages.home._blogs')
+			@include('pages.home._feature')
+			@include('pages.home._brands')
+			@include('pages.home._blogs')
 
-        </main>
-        <!-- /main -->
+		</main>
+		<!-- /main -->
 
 
 
